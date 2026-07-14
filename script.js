4f054195ee64b663d68b35c9d4f7b03d07126914
mhalikahi84-dot <mhalikahi84@gmail.com>
2026-07-14 12:37:16 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu a");
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
+
+    if (menu.classList.contains("active")) {
+        menuToggle.innerHTML = "✕";
+        document.body.style.overflow = "hidden";
+    } else {
+        menuToggle.innerHTML = "☰";
+        document.body.style.overflow = "";
+    }
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+        menuToggle.innerHTML = "☰";
+        document.body.style.overflow = "";
+    });
 });
